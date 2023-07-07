f41e6831ef65a00caa5641176b96ebb795b0b7b2
Sardor <salimovone@gmail.com>
2023-07-07 15:54:40 +0500

    facts created

---
+++ src/components/Facts/style.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+import img from '../../assets/images/about.jpg'
+
+
+export const Main = styled.div`
+    .left{
+        background-image: url(${img});
+        background-position: top;
+        background-size: cover;
+    }
+`
