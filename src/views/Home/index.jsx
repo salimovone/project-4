1237bbcd19a96a6aad534141d1c53af67eb55875
Sardor <salimovone@gmail.com>
2023-07-08 14:20:20 +0500

    carousel 1

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/Header'
 import Services from '../../components/Services'
 import Facts from '../../components/Facts'
+import Carousel from '../../components/Carousel'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <Services />
         <Facts />
+        <Carousel />
     </div>
   )
 }
